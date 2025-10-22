73000de22e2a1b21b372317a358bbd814b865b4f
William Y. Feng <womogenes2@gmail.com>
2025-10-22 17:41:24 -0400

    Weak, half-finished attempt at best response implementation

--- pset2/parser.py
+++ pset2/parser.py
@@ -1,5 +1,7 @@
 import numpy as np
 from pprint import pprint
+from collections import defaultdict
+from functools import lru_cache
 
 class Game:
     def __init__(self, game_file: str):
@@ -67,11 +69,11 @@ class Game:
                 else:
                     assert node_type == "chance"
                     actions = []
-                    probs = []
+                    probs = {}
                     for action in parts[4:]:
                         act, p = action.split("=")
                         actions.append(act)
-                        probs.append(float(p))
+                        probs[act] = float(p)
 
                     nodes[hist] = {
                         "type": "chance",
@@ -83,12 +85,12 @@ class Game:
                 assert parts[0] == "infoset"
                 # Looks like "infoset /P1:?/ nodes /P1:r/ /P1:p/ /P1:s/"
                 info_hist = parts[1]
-                info_nodes = parts[3:]
+                subnodes = parts[3:]
 
                 actions = None
                 player = None
 
-                for node_hist in info_nodes:
+                for node_hist in subnodes:
                     node = nodes[node_hist]
                     assert node["type"] == "player"
                     
@@ -107,7 +109,7 @@ class Game:
 
                 infosets[info_hist] = {
                     "player": player,
-                    "nodes": tuple(info_nodes),
+                    "nodes": tuple(subnodes),
                     "actions": actions,
                 }
 
@@ -117,9 +119,14 @@ class Game:
         self.n_players = len(self.players)
         self.player2idx = {player: idx for idx, player in enumerate(self.players)}
 
+        assert self.n_players == 2
+        assert self.players == ["1", "2"]
+
+    @lru_cache(None)
     def get_children(self, node_hist: str):
         """
         Given a node history label in the game tree, generate list of children
+        Returns a list of (action, child_node_hist) pairs
         """
         assert node_hist in self.nodes
         node = self.nodes[node_hist]
@@ -141,6 +148,10 @@ class Game:
         Given we already have self.nodes and self.infosets, generate
             list of sequences with parent relationships
         """
+        # Enumerate all sequences by player
+        # Deterministic, ordered, etc
+        all_seqs = [set() for _ in range(self.n_players)]
+
         # Stores (hist, (most_recent_seqs))
         stack = [("/", (None,) * self.n_players)]
         visited = set()
@@ -172,6 +183,7 @@ class Game:
             for action, child_hist in children:
                 # Mark parent sequence
                 seq = (node["infoset"], action)
+                all_seqs[player_idx].add(seq)
                 par_seq[seq] = last_seqs[player_idx]
 
                 # DFS
@@ -180,19 +192,151 @@ class Game:
                 stack.append((child_hist, tuple(new_last_seqs)))
         
         self.par_seq = par_seq
+        self.all_seqs = [sorted(seqs) for seqs in all_seqs]
+        self.seq2idx = [{
+            seq: idx for idx, seq in enumerate(seqs)
+        } for seqs in self.all_seqs]
+
+    def gen_uniform_strat(self, player: str):
+        """
+        Given player string, e.g. '1' or '2', generate uniform strategy
+        Strategy is represented in behavioral form (oops)
+        """
+        strat = {}
+        infoset_hists = [
+            hist for hist, infoset in self.infosets.items() if infoset["player"] == player]
+
+        for infoset_hist in infoset_hists:
+            infoset = self.infosets[infoset_hist]
+            if infoset["player"] != player:
+                continue
+            actions = infoset["actions"]
+            strat[infoset_hist] = {action: 1 / len(actions) for action in actions}
+
+        return strat
+
+    def get_best_response(self, player: str, opp_strat: list):
+        """
+        Finds best response for given player, knowing other player's
+            strategy specification.
+        """
+        # Step 1: get "counterfactual probabilities" for each node
+        # And then renormalize within infosets
+        # Top-down recursion
+        # NOTE: this is conditional on other player's strat
+        cf_prob = {}
+        stack = [("/", 1.0)]
+        while len(stack) > 0:
+            node_hist, cur_prob = stack.pop()
+
+            if node_hist in cf_prob:
+                continue
+            cf_prob[node_hist] = cur_prob
+
+            node = self.nodes[node_hist]
+            if node["type"] == "player":
+                if node["player"] == player:
+                    # Play everything without modify probabilities
+                    for _, child_hist in self.get_children(node_hist):
+                        stack.append((child_hist, cur_prob))
+                else:
+                    # Play everything, DO modify probabilities based on other player's strat
+                    for action, child_hist in self.get_children(node_hist):
+                        stack.append((
+                            child_hist,
+                            cur_prob * opp_strat[node["infoset"]][action]
+                        ))
+
+            elif node["type"] == "chance":
+                for action, child_hist in self.get_children(node_hist):
+                    stack.append((
+                        child_hist,
+                        cur_prob * node["probs"][action]
+                    ))
+
+        print(f"{cf_prob=}")
+
+        # Step 2: make strategy for player 1
+        strat = {}
+
+        # Utility per node
+        @lru_cache(None)
+        def get_util(infoset_hist):
+            """
+            Get utility of given infoset.
+            """
+            infoset = self.infosets[infoset_hist]
+
+            # For each *action*, figure out the expected utility
+            E_util = defaultdict(float)
+            for subnode_hist in infoset["nodes"]:
+                subnode_p = cf_prob[subnode_hist]
+                for action, child_hist in self.get_children(subnode_hist):
+                    child_node = self.nodes[child_hist]
+                    if child_node["type"] == "terminal":
+                        E_util[action] += child_node["payoffs"][player] * subnode_p
+                        continue
+                    elif child_node["type"] == "chance":
+                        pass
+
+                    # Simulate what opponent would do
+                    for opp_action, opp_child_hist in self.get_children(child_hist):
+                        p = subnode_p * opp_strat[child_hist][opp_action]
+                        opp_child = self.nodes[opp_child_hist]
+                        
+                        # If terminal, pays off
+                        if opp_child["type"] == "terminal":
+                            E_util[action] += opp_child["payoffs"][player] * p
+                        else:
+                            E_util[action] += get_util(opp_child["infoset"]) * p
+
+            # E_util reports exp. utility of playing each action at infoset
+            # Pick action with highest expected utility
+            E_util_sorted = sorted(E_util.items(), key=lambda a, util: -util)
+            best_action, exp_util = E_util_sorted[0]
+
+            strat[infoset_hist] = best_action
+            return exp_util
+        
+        return get_util("/")
+
+    """
+    Known:
+        5.1:
+            nash gap of both uniform:
+                rpss: 2/3
+                kuhn: 0.916 (truncated, not rounded)
+                leduc: 4.74 (truncated)
+        5.2:
+            plots achieve best response values
+        5.3:
+            nash gaps should be approx. 0
+            value of game (p1):
+                rpss: 0
+                kuhn: -0.05
+                leduc: -0.80??
+    """
 
 
 if __name__ == "__main__":
     game = Game("./efgs/kuhn.txt")
 
-    print("===== NODES =====")
-    pprint(game.nodes)
-    print()
-    print("===== INFOSETS =====")
-    pprint(game.infosets)
-    print()
-    print("===== PLAYERS =====")
-    print(game.players)
-    print()
-    print("===== PAR_SEQ =====")
-    pprint(game.par_seq)
+    # print("===== NODES =====")
+    # pprint(game.nodes)
+    # print()
+    # print("===== INFOSETS =====")
+    # pprint(game.infosets)
+    # print()
+    # print("===== PLAYERS =====")
+    # print(game.players)
+    # print()
+    # print("===== PARARENT SEQUENCES =====")
+    # pprint(game.par_seq)
+    # print("===== ALL SEQUENCES =====")
+    # pprint(game.all_seqs)
+
+    uniform_2 = game.gen_uniform_strat("2")
+    print(f"{uniform_2=}")
+
+    br_1 = game.get_best_response("1", uniform_2)
+    print(br_1)
